164f5a6b1deb54e4b288d7e7b734f524e1ea9815
dudesahn <dudesahn@gmail.com>
2022-08-23 21:59:25 -0400

    fix: update to deposit with WETH instead of WBTC

--- contracts/StrategyCurve3Crypto.sol
+++ contracts/StrategyCurve3Crypto.sol
@@ -202,7 +202,6 @@ contract StrategyCurve3Crypto is StrategyCurveBase {
         // these are our standard approvals. want = Curve LP token
         want.approve(address(proxy), type(uint256).max);
         crv.approve(address(crveth), type(uint256).max);
-        weth.approve(uniswapv3, type(uint256).max);
 
         // this is the pool specific to this vault
         curve = ICurveFi(_curvePool);
@@ -217,10 +216,7 @@ contract StrategyCurve3Crypto is StrategyCurveBase {
         stratName = _name;
 
         // these are our approvals and path specific to this contract
-        wbtc.approve(address(curve), type(uint256).max);
-
-        // set our uniswap pool fees
-        uniWbtcFee = 500;
+        weth.approve(address(curve), type(uint256).max);
     }
 
     /* ========== MUTATIVE FUNCTIONS ========== */
@@ -255,10 +251,10 @@ contract StrategyCurve3Crypto is StrategyCurveBase {
         // do this even if we don't have any CRV, in case we have WETH
         _sell(_crvBalance);
 
-        // deposit our balance to Curve if we have any
-        uint256 _wbtcBalance = wbtc.balanceOf(address(this));
-        if (_wbtcBalance > 0) {
-            curve.add_liquidity([0, _wbtcBalance, 0], 0);
+        // deposit our assets to the pool
+        uint256 wethBalance = weth.balanceOf(address(this));
+        if (wethBalance > 0) {
+            curve.add_liquidity([0, 0, wethBalance], 0);
         }
 
         // debtOustanding will only be > 0 in the event of revoking or if we need to rebalance from a withdrawal or lowering the debtRatio
@@ -305,29 +301,11 @@ contract StrategyCurve3Crypto is StrategyCurveBase {
         crv.safeTransfer(_newStrategy, crv.balanceOf(address(this)));
     }
 
-    // Sells our harvested CRV into ETH then WBTC
+    // Sells our harvested CRV into WETH
     function _sell(uint256 _crvAmount) internal {
         if (_crvAmount > 1e17) {
             // don't want to swap dust or we might revert
             crveth.exchange(1, 0, _crvAmount, 0, false);
-        }
-
-        uint256 _wethBalance = weth.balanceOf(address(this));
-        if (_wethBalance > 1e15) {
-            // don't want to swap dust or we might revert
-            IUniV3(uniswapv3).exactInput(
-                IUniV3.ExactInputParams(
-                    abi.encodePacked(
-                        address(weth),
-                        uint24(uniWbtcFee),
-                        address(wbtc)
-                    ),
-                    address(this),
-                    block.timestamp,
-                    _wethBalance,
-                    uint256(1)
-                )
-            );
         }
     }
 
